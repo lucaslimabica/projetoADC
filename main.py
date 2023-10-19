73101bfa50cd814cae85d765b4667dee40c66d93
Lucas Bica <a82451@ualg.pt>
2023-10-19 14:41:54 +0100

    Adicionei uma função que lê floats

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from get_int import *
+from get_float import *
+
+# Atividade de ADC
+
 if __name__ == '__main__':
-	print('Hello World!")
+	x = get_int("x: ")
+	i = get_int("i: ")
+	z = x*y
+	print(z)
